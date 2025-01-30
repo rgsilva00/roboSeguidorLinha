180dd449932451fc3af4470915a582659b7c8c76
Rogerio Silva <rgsilva00@users.noreply.github.com>
2025-01-30 13:36:59 -0300

    Código_Carro_Seguidor_Linha
    O código apresentado está em sua versão 0.1, antes de ser implementado ao hardware do Arduino e realizado os devidos ajustes complementares no código para rodar em conformidade com o Arduino.

---
+++ car_seguidor_linha.ino
@@ -0,0 +1,69 @@
+//Definição dos pinos dos sensores
+#define pinSensorD 2
+#define pinSensorE 3
+
+//Definição dos pinis dos motores
+//Motor 1
+#define pinMotorD1 7
+#define pinMotorD2 6
+//Motor 2
+#define pinMotorE1 5
+#define pinMotorE2 4
+
+//Definição da variável de velocidade
+#define velocidade = 70
+
+void setup() {
+  //Os pinos dos sensonres apontados como ENTRADA
+  pinMode(pinSensorD, INPUT);
+  pinMode(pinSensorE, INPUT);
+
+  //Os pinos de controle do motor apontados como SAÍDA
+  pinMode(pinMotorD1, OUTPUT);
+  pinMode(pinMotorD2, OUTPUT);
+  pinMode(pinMotorE1, OUTPUT);
+  pinMode(pinMotorE2, OUTPUT);
+}
+
+void loop() {
+  // Leitura dos sensores infravermelhos
+  int leitorE = digitalRead(pinSensorE);
+  int leitorD = digitalRead(pinSensorD);
+
+  //Caso os dois sensores de linha detectem a linha (ambos os sensores na linha)
+  if (leitorE == LOW && leitorD == LOW) {
+    //motor 1
+    digitalWrite(pinMotorD1, velocidade);
+    digitalWrite(pinMotorD2, LOW);
+    //motor 2
+    digitalWrite(pinMotorE1, velocidade);
+    digitalWrite(pinMotorE2, LOW);
+  }
+  //Caso Caso o sensor da esquerda detecte a linha (ajustar para a esquerda)
+  else if (leitorE == LOW && leitorD == HIGH) {
+    //motor 1
+    digitalWrite(pinMotorD1, LOW);
+    digitalWrite(pinMotorD2, velocidade);
+    //motor 2
+    digitalWrite(pinMotorE1, velocidade);
+    digitalWrite(pinMotorE2, LOW);
+  }
+  // Caso o sensor da direita detecte a linha (ajustar para a direita)
+  else if (leitorE == HIGH && leitorD == LOW) {
+    //motor 1
+    digitalWrite(pinMotorD1, velocidade);
+    digitalWrite(pinMotorD2, LOW);
+    //motor 2
+    digitalWrite(pinMotorE1, LOW);
+    digitalWrite(pinMotorE2, velocidade);
+  }
+  // Caso nenhum sensor detecte a linha (parar ou tomar uma ação específica)
+  else {
+    //motor 1
+    digitalWrite(pinMotorD1, LOW);
+    digitalWrite(pinMotorD2, LOW);
+    //motor 2
+    digitalWrite(pinMotorE1, LOW);
+    digitalWrite(pinMotorE2, LOW);
+  }
+}
